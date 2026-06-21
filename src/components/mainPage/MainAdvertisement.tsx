1358f1399542a01b762315ac2fea01da1fff0734
tsult595 <teymurt@hotmail.com>
2026-06-21 13:40:18 -0700

    ddd

--- src/components/mainPage/MainAdvertisement.tsx
+++ src/components/mainPage/MainAdvertisement.tsx
@@ -4,6 +4,7 @@ const MainAdvertisement = () => {
   return (
       <div className="flex-1 w-10 h-100 bg-amber-200 m-4 p-4 rounded-lg shadow">
         <h2 className="text-2xl font-bold mb-4">Main Advertisement</h2>
+        
     </div>
   )
 }
