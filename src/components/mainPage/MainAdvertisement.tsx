e01efacff8a0830747f9c61c8d1d72e1834d3a0c
tsult595 <teymurt@hotmail.com>
2026-06-27 16:41:20 -0700

    ready)

--- src/components/mainPage/MainAdvertisement.tsx
+++ src/components/mainPage/MainAdvertisement.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useTranslations} from 'next-intl';
 
 const MainAdvertisement = () => {
+   const t = useTranslations('advertisement');
   return (
       <div className="flex-1 w-10 h-100 bg-amber-200 m-4 p-4 rounded-lg shadow">
-        <h2 className="text-2xl font-bold mb-4">Main Advertisement</h2>
+        <h2 className="text-2xl font-bold mb-4">{t('title')}</h2>
+        <p className="text-gray-600">{t('description')}</p>
     </div>
   )
 }
--- src/components/mainPage/MainAdvertisementSmall.tsx
+++ src/components/mainPage/MainAdvertisementSmall.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useTranslations} from 'next-intl';
 
 const MainAdvertisementSmall = () => {
+  const t = useTranslations('advertisement');
   return (
     <div className="bg-gray-200 mt-4 h-100 w-1/3">
-      <h3>Main Advertisement Small</h3>
+      <h3>{t('title')}</h3>
+      <p>{t('description')}</p>
     </div>
   )
 }
